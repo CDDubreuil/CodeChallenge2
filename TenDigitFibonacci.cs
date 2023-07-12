a00b94466089d96cd40686dda57a82f2d6e48961
Chanel Dubreuil <cddubreuil@outlook.com>
2023-07-12 17:47:19 -0500

    Copy and pasted my previous Multiples3sAnd5s challenge project over so it could be easier to keep together. I messed up the first repository somehow. Also did the Ten Digit and Even Sum Fibonacci projects. Still trying to figure out the Collatz stuff though, having a hard time on that one.

---
+++ TenDigitFibonacci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge2
+{
+    internal class TenDigitFibonacci
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===== Fibonacci Ten Digits ===== ");
+
+          
+            long numDigits = 0;
+            long num1 = 1;
+            long num2 = 1;
+            long tempNum = 0;
+
+           
+
+            while (num2 < 10000000001)
+            {
+                
+                if (num2 < 1000000000)
+                {
+                    numDigits += num2; 
+                }
+              tempNum = num2;
+                num2 = (num1 + num2);
+                num1= tempNum;
+
+
+            }
+            Console.WriteLine("The first number in the Fibonacci sequence to get to ten digits is " + numDigits);
+
+        }
+    }
+}
