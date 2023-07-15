82f74d35238a346096bc50efc476a8f6e486d29e
Chanel Dubreuil <cddubreuil@outlook.com>
2023-07-15 12:40:19 -0500

    Collatz

--- LongestCollatz.cs
+++ LongestCollatz.cs
@@ -1,10 +1,12 @@
-﻿namespace CodeChallenge2
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge2
 {
-    internal class Program
+    internal class LongestCollatz
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
     }
 }
